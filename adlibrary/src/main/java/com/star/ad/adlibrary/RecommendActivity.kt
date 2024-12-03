7772178bb3f72f308b4714b2a06ab45f5e4b1452
V.shengcai.ning <you@example.com>
2024-12-04 00:36:00 +0800

    [ad]modify bug

--- adlibrary/src/main/java/com/star/ad/adlibrary/RecommendActivity.kt
+++ adlibrary/src/main/java/com/star/ad/adlibrary/RecommendActivity.kt
@@ -1,10 +1,8 @@
 package com.star.ad.adlibrary
 
-import android.R
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
