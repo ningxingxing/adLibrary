50f131315e8659bef423b0058ce975935a1c2d2d
V.shengcai.ning <you@example.com>
2024-12-04 21:50:47 +0800

    [ads]修改图标颜色

--- adlibrary/src/main/java/com/star/ad/adlibrary/helper/RecommendHelper.kt
+++ adlibrary/src/main/java/com/star/ad/adlibrary/helper/RecommendHelper.kt
@@ -1,12 +1,30 @@
 package com.star.ad.adlibrary.helper
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import com.star.ad.adlibrary.R
+import com.star.ad.adlibrary.RecommendActivity
 
 object RecommendHelper {
+
+    private const val TAG = "RecommendHelper"
 
     val icons = arrayOf(R.drawable.eq,R.drawable.music,R.drawable.photo)
     val titles = arrayOf(R.string.eq_name,R.string.music_name,R.string.photo_name)
     val urls = arrayOf(R.string.eq_url,R.string.music_url,R.string.photo_url)
 
 
+    fun openApp(context: Context,url: String) {
+        try {
+            val uri =
+                Uri.parse(url)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            context.startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Log.e(TAG, "share =${e.message}")
+        }
+    }
 }
